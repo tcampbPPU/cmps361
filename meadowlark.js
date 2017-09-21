8fad01e339398d2c92dfe1b0819070695fa4a8b9
Tanner Campbell <tcamp022@gmail.com>
2017-09-21 11:53:16 -0400

    fortune js

--- meadowlark.js
+++ meadowlark.js
@@ -1,4 +1,6 @@
+// New fortunes at lib/fortune.js
 var express = require('express');
+var fortune = require('./lib/fortune.js');
 var app = express();
 var handlebars = require('express-handlebars').create({ defaultLayout:'main' });
 
@@ -9,20 +11,14 @@ app.set('view engine', 'handlebars');
 // Set Port
 app.set('port', process.env.PORT || 3000);
 
+// Chai and Mocha Test
+app.use(function(req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 // Looks for files in Public Dir
 app.use(express.static(__dirname + '/public'));
-
-// add fortunes for dynamic test
-var fortuneCookies = [
-	"Conquer your fears or they will conquer you.",
-	"Rivers need springs.",
-	"Do not fear what you don't know.",
-	"You will have a pleasant surprise.",
-	"Whenever possible, keep it simple.",
-];
-
-// var date = new Date();
-// console.log(date);
 
 // Render looks in views for file name in extenstion
 
@@ -37,10 +33,14 @@ app.get('/datetime', function(req, res) {
   res.render('datetime', { datetime: date});
 });
 
-// About Dir
+//Old About Dir
+//app.get('/about', function(req,res){
+//   res.render('about', { fortune: fortune.getFortune() });
+//});
+
+// About Dir with Chai and Mocha
 app.get('/about', function(req,res){
-  var randomFortune = fortuneCookies[Math.floor(Math.random() * fortuneCookies.length)];
-  res.render('about', { fortune: randomFortune });
+  res.render('about', { fortune: fortune.getFortune(), pageTestScript: '/qa/tests-about.js'});
 });
 
 
@@ -56,7 +56,17 @@ app.use(function(err, req, res, next){
   res.type('text/ plain'); 
   res.status(500);
   res.render('500');
-}); 
+});
+
+// hood-river
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+// request group rate
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
+});
 
 // Listen to APP for Port
 app.listen(app.get('port'), function(){ console.log('Express started on http:// localhost:' + app.get('port') + '; press Ctrl-C to terminate.'); 
